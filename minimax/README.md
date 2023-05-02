5fb03efce368b1bb0e326e0b13a3c39638fc2ef1
RaquelBehrens <raquel.behrens@hotmail.com>
2023-05-01 22:09:07 -0300

    Finishing trail.py and report

--- minimax/README.md
+++ minimax/README.md
@@ -1,16 +1,132 @@
-00 01 02 03 04 05 06 07 08 09
-10 11 12 13 14 15 16 17 18 19
-20 21 22 23 24 25 26 27 28 29
-30 31 32 33 34 35 36 37 38 39
-40 41 42 43 44 45 46 47 48 49
-50 51 52 53 54 55 56 57 58 59
-60 61 62 63 64 65 66 67 68 69
-70 71 72 73 74 75 76 77 78 79
-80 81 82 83 84 85 86 87 88 89
-90 91 92 93 94 95 96 97 98 99
+# Relatório
 
-00 01 02 03 04
-05 06 07 08 09
-10 11 12 13 14
-15 16 17 18 19
-20 21 22 23 24
+### Bruna Carpes de Freitas
+
+### Larissa Gremelmaier Rosa
+
+### Maren Freland Arn
+
+### Raquel Cristina Schaly Behrens
+
+The group used the framework [EasyAI](https://github.com/Zulko/easyAI), see further documentation [here](http://zulko.github.io/easyAI/), to develop our version of the Dots Game and use artificial inteligence to play against the computer, or observe the computer play with itself.
+
+### But how does our Dots Game work?
+
+You have a board with a certain amount of dots, for example, a 5x5 board with dots in each position, like this:
+
+. . . . . 00 01 02 03 04
+. . . . . 05 06 07 08 09
+. . . . . 10 11 12 13 14
+. . . . . 15 16 17 18 19
+. . . . . 20 21 22 23 24
+
+Therefore, you have to draw edges between the dots, to form boxes. Each box your edges form, will be a point for you. But all the edges of the box must be yours.
+
+Our game uses AI that thinks 5 moves in advance (line 110 of /trail.py), and uses the [Negamax](https://en.wikipedia.org/wiki/Negamax) algorithm, which is a variant from **minimax** search, that relies on the zero-sum property of a two-player game.
+
+So let's play!
+
+##
+
+### How do I execute the file?
+
+You must have python3 installed in your computer.
+
+Also, you will need easyAI installed. You can prompt the following command:
+
+`sudo pip install easyAI`
+
+Now you must prompt, in this directory:
+
+`python3 trail.py`
+
+Now you must choose an option between:
+
+1 - Play with AI
+2 - Observe 2 AIs play with each other
+3 - Solve with iterative deepening (i.e. combine breadth-first search with depth-first search)
+
+To see further meaning of iterative deepening of easyAI's function, please access easyAI [documentation](https://github.com/Zulko/easyAI).
+
+##
+
+## Choosing option 1: Play with AI
+
+You are the player 2.
+
+First you have to type the size of the board. We recommend 3.
+
+Let's say you typed 3. You have the following board:
+
+. . . 00 01 02
+. . . 03 04 05
+. . . 06 07 08
+
+The player 1 will start (the AI).
+
+Now it's your turn.
+
+To choose an edge, if the edge is from 00 to 01, you should type:
+
+`(0, 1)`
+
+**Do not forget the space between ',' and the next number, in this case, '1'.**
+
+And then you will be able to see your play in the terminal.
+
+When you finish the game, the board will inform who won. If there is a tie, you lost.
+
+##
+
+## Choosing option 2: Observe 2 AIs play with each other
+
+First you have to type the size of the board. We recommend 3.
+
+Let's say you typed 3. You have the following board:
+
+. . . 00 01 02
+. . . 03 04 05
+. . . 06 07 08
+
+Now you can observe in the temrinal who won the game, and the moves of the players!
+
+##
+
+## Choosing option 3: Solve with iterative deepening
+
+To see further meaning of iterative deepening of easyAI's function, please access easyAI [documentation](https://github.com/Zulko/easyAI).
+
+But there, it says:
+
+We obtain r=1, meaning that if both players play perfectly, the first player to play can always win (-1 would have meant always lose), d=10, which means that the wins will be in ten moves (i.e. 5 moves per player) or less, and m='3', which indicates that the first player's first move should be '3'.
+
+Therefore, first you have to type the size of the board. We recommend 3.
+
+Now you can observe r, d and m appearing in the terminal.
+
+For a board of size 3x3, we have the following result:
+
+d:2, a:0, m:(0, 1)
+d:3, a:0, m:(0, 1)
+d:4, a:0, m:(0, 1)
+d:5, a:0, m:(0, 1)
+d:6, a:0, m:(0, 1)
+d:7, a:0, m:(0, 1)
+d:8, a:0, m:(0, 1)
+d:9, a:0, m:(0, 1)
+d:10, a:0, m:(0, 1)
+d:11, a:0, m:(0, 1)
+d:12, a:0, m:(0, 1)
+d:13, a:0, m:(0, 1)
+d:14, a:0, m:(0, 1)
+d:15, a:0, m:(0, 1)
+d:16, a:0, m:(0, 1)
+d:17, a:0, m:(0, 1)
+d:18, a:0, m:(0, 1)
+d:19, a:0, m:(0, 1)
+
+r: 0
+d: 19
+m: (0, 1)
+
+Meaning that the first player cannot always win, the wins will be in 19 moves, and the first player's move should be **(0, 1)**.
--- minimax/trail.py
+++ minimax/trail.py
@@ -1,64 +1,62 @@
-from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
+from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax, SSS
+from easyAI import solve_with_iterative_deepening
+
 from colorama import Fore, Back, Style
 
-SIZE = 5
 
-if SIZE == 5:
-    TOTAL = 25 - 1
-    LAST_LINE = 20
-    LAST_COLUMN = 4
-
-elif SIZE == 10:
-    TOTAL = 100 - 1
-    LAST_LINE = 90
-    LAST_COLUMN = 9
-
-
-class GameOfDots( TwoPlayerGame ):
-    def __init__(self, players=None):
+class GameOfDots(TwoPlayerGame):
+    def __init__(self, players=None, sizeOfBoard = 3):
+        self.SIZE = sizeOfBoard
+        self.TOTAL = (self.SIZE*self.SIZE) - 1
+        self.LAST_LINE = (self.TOTAL+1)-self.SIZE
+        self.LAST_COLUMN = self.SIZE-1
+        
         self.players = players
         self.board = self.create_board()
         self.current_player = 1 # player 1 starts
-        self.player_1_moves = []
-        self.player_2_moves = []
+
+        self.player_1_moves = [[] for i in range(self.TOTAL+1)]
+        self.player_2_moves = [[] for i in range(self.TOTAL+1)]
+
 
     def create_board(self):
-        return [(i, i+1) for i in range(TOTAL) if i % SIZE != LAST_COLUMN] + \
-        [(i, i+SIZE) for i in range(LAST_LINE)] 
+        return [(i, i+1) for i in range(self.TOTAL) if i % self.SIZE != self.LAST_COLUMN] + \
+        [(i, i+self.SIZE) for i in range(self.LAST_LINE)] 
 
-    def possible_moves(self): return self.board
-    def make_move(self, move): 
+    def possible_moves(self):
+        return self.board #possible moves are stored in self.board
+
+    def make_move(self, move):
         self.board.remove(move)
+        x, y = move
         if self.current_player == 1:
-            self.player_1_moves.append(move)
+            self.player_1_moves[x].append(y)
         else:
-            self.player_2_moves.append(move)
+            self.player_2_moves[x].append(y)
 
-    def win(self): (not bool(self.board)) and (self.scoring < 0) # opponent took the last bone ?
+    def win(self): return self.scoring() > 0 # opponent took the last bone ?
 
-    def is_over(self): return self.win() # Game stops when someone wins.
+    def is_over(self): return not bool(self.board) # Game stops when board is empty, i.e. there is no other possible moves
 
     def show(self): 
-        for i in range(SIZE):
-            for j in range(SIZE):
+        for i in range(self.SIZE):
+            for j in range(self.SIZE):
                 print(Fore.RESET + '.', end='')
-                number = (i*SIZE)+j
-                edge = (number, number+1)
-                if (edge in self.player_1_moves):
-                    print(Fore.RED + '-', end='')
-                elif (edge in self.player_2_moves):
+                number = (i*self.SIZE)+j
+                if (number+1 in self.player_1_moves[number]):
                     print(Fore.BLUE + '-', end='')
+                elif (number+1 in self.player_2_moves[number]):
+                    print(Fore.RED + '-', end='')
                 else:
                     print(' ', end='')
             
             print()
-            for j in range(SIZE):
-                number = (i*SIZE)+j
-                edge = (number, number+SIZE)
-                if (edge in self.player_1_moves):
+            for j in range(self.SIZE):
+                number = (i*self.SIZE)+j
+                if (number+self.SIZE in self.player_1_moves[number]):
+                    print(Fore.BLUE + '|', end='')
+                elif (number+self.SIZE in self.player_2_moves[number]):
                     print(Fore.RED + '|', end='')
-                elif (edge in self.player_2_moves):
-                    print(Fore.BLUE, '|', end='')
                 else:
                     print(' ', end='')
                 print(' ', end='')
@@ -68,20 +66,75 @@ class GameOfDots( TwoPlayerGame ):
     def scoring(self): 
         score = 0
         for i in range(len(self.player_1_moves)):
-            move = self.player_1_moves[i]
-            x, y = move
-            if ((x, x+SIZE) in self.player_1_moves) and ((y, y+SIZE) in self.player_1_moves) and ((x+SIZE,y+SIZE) in self.player_1_moves):
-                score -= 1
+            moves = self.player_1_moves[i]
+            if ((i+1) in moves) and ((i+self.SIZE) in moves):
+                if (i+self.SIZE+1) in self.player_1_moves[i+1]:
+                    if (i+self.SIZE+1) in self.player_1_moves[i+self.SIZE]:
+                        score += 1
 
         for i in range(len(self.player_2_moves)):
-            move = self.player_2_moves[i]
-            x, y = move
-            if ((x, x+SIZE) in self.player_2_moves) and ((y, y+SIZE) in self.player_2_moves) and ((x+SIZE,y+SIZE) in self.player_2_moves):
-                score += 1
+            moves = self.player_2_moves[i]
+            if ((i+1) in moves) and ((i+self.SIZE) in moves):
+                if (i+self.SIZE+1) in self.player_2_moves[i+1]:
+                    if (i+self.SIZE+1) in self.player_2_moves[i+self.SIZE]:
+                        score -= 1
 
-        return score
+        if score > 0:
+            return 100
+        else:
+            return 0
 
-# Start a match (and store the history of moves when it ends)
-ai = Negamax(1) # The AI will think 13 moves in advance
-game = GameOfDots( [ Human_Player(), AI_Player(ai) ] )
-history = game.play()
+    def lose(self):
+        return self.scoring() <= 0
+    
+
+if __name__ == "__main__":
+    print("Hello! Welcome to the gem of dots!")
+
+    execute = True
+    while execute:
+        print()
+        print("Please select an option: ")
+        print("1 - Play with IA")
+        print("2 - Observe 2 IAs play with each other")
+        print("3 - Solve with iterative deepening (i.e. combine breadth-first search with depth-first search)")
+        print("To see further meaning of iterative deepening, please access its documentation, in this directory's README")
+        print("Other - Exit")
+        option = int(input())
+        print()
+
+        if option > 3 or option <= 0:
+            execute = False
+            break
+
+        size = int(input("Please type the size of board you wish. We recommend 3: "))
+
+        ai1 = Negamax(5) # The AI will think 5 moves in advance
+        ai2 = Negamax(1)
+        
+        if option == 3:
+            r,d,m = solve_with_iterative_deepening(
+                game=GameOfDots(size),
+                ai_depths=range(2,20),
+                win_score=100
+            )
+
+            print("r: ", end='')
+            print(r)
+            print("d: ", end='')
+            print(d)
+            print("m: ", end='')
+            print(m)
+                        
+        else:
+            if option == 1:
+                game = GameOfDots( [ AI_Player(ai1), Human_Player() ] , sizeOfBoard=3)
+
+            elif option == 2:
+                game = GameOfDots( [ AI_Player(ai1), AI_Player(ai2) ] , sizeOfBoard=3)
+
+            game.play()
+            if game.lose():
+                print("Player %d wins." % (game.current_player))
+            else:
+                print("Looks like we have a draw.")
